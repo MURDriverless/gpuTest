4e02702328a2ad4d16dc9cc63a28708b787b5e09
Andrew Huang <huanga2@student.unimelb.edu.au>
2020-10-24 20:50:47 +1100

    init

---
+++ readme.md
@@ -0,0 +1,6 @@
+# GpuTest
+Simple Script for testing GPU vs CPU perf in OpenCV `colorCvt` and `remap`
+
+## Requirements
+ - `CUDA 10.0`
+ - `OpenCV 4.1.1`
---
+++ src/main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <chrono>
+
+#include <opencv2/core.hpp>
+#include <opencv2/core/persistence.hpp>
+#include <opencv2/calib3d.hpp>
+
+#include <opencv2/core/cuda.hpp>
+#include <opencv2/cudawarping.hpp>
+#include <opencv2/cudaarithm.hpp>
+#include <opencv2/cudaimgproc.hpp>
+
+#define RUN_SIZE 10
+
+int main(int argc, char** argv) {
+    std::cout << "Hello World" << std::endl;
+
+    cv::Mat cameraMatrix, distCoeffs;
+    cv::Size calibSize;
+
+    cv::FileStorage fs;
+    fs.open("../calibration.xml", cv::FileStorage::READ);
+    fs["cameraMatrix"]   >> cameraMatrix;
+    fs["distCoeffs"]     >> distCoeffs;
+    fs["calibImageSize"] >> calibSize;
+
+    cv::Mat xmap, ymap;
+    cv::cuda::GpuMat xmap_gpu, ymap_gpu;
+
+    cv::Mat newCameraMatrix = cv::getOptimalNewCameraMatrix(cameraMatrix, distCoeffs, calibSize, 0.0);
+    cv::initUndistortRectifyMap(cameraMatrix, distCoeffs, cv::Mat(), newCameraMatrix, cv::Size(1920, 1200), CV_32FC1, xmap, ymap);
+    xmap_gpu.upload(xmap);
+    ymap_gpu.upload(ymap);
+
+    cv::Mat testImage(calibSize, CV_8UC1);
+    cv::Mat testImageColor(calibSize, CV_8UC3);
+    cv::Mat testImageColor_Undist(calibSize, CV_8UC3);
+
+    cv::cuda::GpuMat testImage_gpu;
+    cv::cuda::GpuMat testImageColor_gpu;
+    cv::cuda::GpuMat testImageColor_Undist_gpu;
+
+    unsigned long cpuMeanTime = 0;
+    for (int i = 0; i < RUN_SIZE; i++) {
+        cv::randu(testImage, 0, 255);
+
+        auto then = std::chrono::high_resolution_clock::now();
+
+        cv::cvtColor(testImage, testImageColor, cv::COLOR_BayerBG2BGR);
+        cv::remap(testImageColor, testImageColor_Undist, xmap, ymap, cv::InterpolationFlags::INTER_CUBIC);
+
+        auto now = std::chrono::high_resolution_clock::now();
+        cpuMeanTime += std::chrono::duration_cast<std::chrono::milliseconds>(now - then).count();
+    }
+
+    unsigned long gpuMeanTime = 0;
+    for (int i = 0; i < RUN_SIZE; i++) {
+        cv::randu(testImage, 0, 255);
+
+        auto then = std::chrono::high_resolution_clock::now();
+
+        testImage_gpu.upload(testImage);
+        cv::cuda::cvtColor(testImage_gpu, testImageColor_gpu, cv::COLOR_BayerBG2BGR);
+        cv::cuda::remap(testImageColor_gpu, testImageColor_Undist_gpu, xmap_gpu, ymap_gpu, cv::InterpolationFlags::INTER_CUBIC);
+        testImageColor_Undist_gpu.download(testImageColor_Undist);
+
+        auto now = std::chrono::high_resolution_clock::now();
+        gpuMeanTime += std::chrono::duration_cast<std::chrono::milliseconds>(now - then).count();
+    }
+
+    std::cout << "CPU mean time taken: " << cpuMeanTime/RUN_SIZE << std::endl;
+    std::cout << "GPU mean time taken: " << gpuMeanTime/RUN_SIZE << std::endl;
+
+    return 0;
+}
